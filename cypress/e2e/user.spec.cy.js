96e8cdebf49688257a8ccd413069d31449d66383
Borgesfabio89 <borges.fabio89@gmail.com>
2025-01-29 15:21:34 -0300

    Add Page Objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,16 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from './../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
 
 describe('Login', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    submitButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-text--h6',
-    dashboradGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialsAlert: '.oxd-alert-content-text',
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
@@ -23,19 +24,17 @@ describe('Login', () => {
   };
   
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.submitButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index' )
-    cy.get(selectorsList.dashboradGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
+    
     cy.get(selectorsList.firstNameField).clear().type('Craque')
-    cy.get(selectorsList.middleNameField).clear().type('Neto')
-    cy.get(selectorsList.lastNameField).clear().type('Garotinho')
+    cy.get(selectorsList.middleNameField).clear().type('Garotinho')
+    cy.get(selectorsList.lastNameField).clear().type('Neto')
     cy.get(selectorsList.genericField).eq(3).clear().type('teste')
     cy.get(selectorsList.genericField).eq(4).clear().type('teste 2')
-    cy.get(selectorsList.genericField).eq(5).clear().type('teste 3')
+    cy.get(selectorsList.genericField).eq(5).clear().type('teste 3')  
     cy.get(selectorsList.genericField).eq(6).clear().type('2025-06-01')
     cy.get(selectorsList.closeDateButton).click()
     cy.get(selectorsList.submitButton).eq(0).click()
